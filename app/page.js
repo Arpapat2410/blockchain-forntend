f77be03517cde7b04ea22eab4cf032b03e4e46fb
Arpapat2410 <120714445+Arpapat2410@users.noreply.github.com>
2023-10-31 17:06:32 +0700

    buy

--- app/page.js
+++ app/page.js
@@ -1,23 +1,33 @@
-"use client"
-import React, {useState , useEffect} from 'react'
-import {initializeConnector} from '@web3-react/core'
-import { MetaMask } from '@web3-react/metamask'
-import { ethers } from 'ethers'
-import { formatEther, parseEther } from '@ethersproject/units'
-import abi from './abi.json'
 
+"use client";
+import React, { useState, useEffect } from "react";
 
-import AppBar from '@mui/material/AppBar';
-import Box from '@mui/material/Box';
-import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
-import Button from '@mui/material/Button';
-import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
+import { initializeConnector } from "@web3-react/core";
+import { MetaMask } from "@web3-react/metamask";
 
-import Chip from '@mui/material/Chip';
-import Stack from '@mui/material/Stack';
-import FaceIcon from '@mui/icons-material/Face';
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
+
+import Chip from "@mui/material/Chip";
+import Stack from "@mui/material/Stack";
+
+import {
+  Container,
+  Card,
+  CardContent,
+  TextField,
+  Divider,
+} from "@mui/material";
+
+// Knowledge about Ether.js https://docs.ethers.org/v6/getting-started/
+import { ethers } from "ethers";
+import { formatEther, parseUnits } from "ethers";
+import abi from "./abi.json";
 
 
 const [ metaMask , hooks ] = initializeConnector((actions) => new MetaMask({ actions }))
@@ -43,15 +53,55 @@ export default function Page() {
   const handleConnect = () => {
     metaMask.activate(contractChain)
   }
-//ยกเลิกการเชื่อมต่อ
+  //ยกเลิกการเชื่อมต่อ
   const handleDisconnect = () => {
     metaMask.resetState()
   }
+
+  const [ balance , setBalance ] = useState("") 
+  useEffect (() => {
+    const fetchBalance = async () => {
+      const signer = provider.getSigner()
+      const smaetContract = new ethers.Contract( contractAddress, abi , signer )
+      const myBalance = await smaetContract.balanceOf(accounts[0])
+      console.log(formatEther(myBalance));
+      setBalance(formatEther(myBalance))
+      
+    }
+    if (isActive) {
+      fetchBalance()
+    }
+  },[isActive])
+
+
   
+  const [aonValue, setAonValue] = useState(0);
+
+  const handleSetAonValue = event => {
+    setAonValue(event.target.value);
+  };
+
+  const handleBuy = async () => {
+    try {
+      if (aonValue <= 0) {
+        return;
+      }
+
+      const signer = provider.getSigner();
+      const smartContract = new ethers.Contract(contractAddress, abi, signer);
+      const buyValue = parseUnits(aonValue.toString(), "ether");
+      const tx = await smartContract.buy({
+        value: buyValue.toString(),
+      });
+      console.log("Transaction hash:", tx.hash);
+    } catch (err) {
+      console.log(err);
+    }
+  };
   return (
-    <>
+
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar  position="static">
+      <AppBar position="static">
         <Toolbar>
           <IconButton
             size="large"
@@ -63,33 +113,79 @@ export default function Page() {
             <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            SE Crypto exchange
           </Typography>
-          
-          { isActive ? 
-          
-          < Stack direction = "row" spacing = {1} alignItems = "center" >
-              <Chip icon = {< FaceIcon / >}label = {accounts ? accounts[0] : ''}variant = "outlined" / >
-              <Button color='inherit' onClick={handleDisconnect} >Disconnect</Button> 
-          </Stack>
-            : 
-              <Button color='inherit' onClick={handleConnect} >Connect</Button>
-          } 
+
+          {isActive ? (
+            <Stack direction="row" spacing={1}>
+              <Chip label={accounts[0]} variant="outlined" />
+              <Button color="inherit" onClick={handleDisconnect}>
+                Disconnect
+              </Button>
+            </Stack>
+          ) : (
+            <Button color="inherit" onClick={handleConnect}>
+              Connect
+            </Button>
+          )}
         </Toolbar>
       </AppBar>
+      {isActive && (
+        <Container maxWidth="sm" sx={{ mt: 2  }}>
+          <Card>
+            <CardContent>
+              <Stack spacing={2}>
+                <Typography
+                  sx={{ fontSize: 20 }}
+                  color="text.secondary"
+                  gutterBottom
+                >
+                  My wallet balance
+                </Typography>
+                <TextField
+                  id="outlined-basic"
+                  label="Address"
+                  value={accounts[0]}
+                  variant="outlined"
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="Aon Balance"
+                  value={balance}
+                  variant="outlined"
+                />
+
+                <Divider />
+                <Typography
+                  sx={{ fontSize: 20 }}
+                  color="text.secondary"
+                  gutterBottom
+                >
+                  Buy Aon Token
+                </Typography>
+
+                <TextField
+                  required
+                  id="outlined-required"
+                  label="Enter amount of Ether you want to buy Aon Token"
+                  defaultValue=""
+                  type="number"
+                  onChange={handleSetAonValue}
+                />
+
+                <Button variant="contained" onClick={handleBuy}>
+                  Buy Aon Token
+                </Button>
+              </Stack>
+            </CardContent>
+          </Card>
+        </Container>
+      )}
     </Box>
-    <div class = "card">
-      <div class="card-body">
-      <p>chainId : { chainId }</p>
-      <p>isActive : { isActive.toString() }</p>
-      <p>accounts : { accounts ? accounts[0] : '' }</p>
-      { isActive ? 
-        <input type='button' class="btn btn-denger" onClick={handleDisconnect} value={'Disconnect'} />
-      :
-        <input type='button'class="btn btn-primary"  onClick={handleConnect} value={'Connect Wallet'} />
-      }
-    </div>
-    </div>
+        
+          
+        
     
-    </>
+
   )
 }
